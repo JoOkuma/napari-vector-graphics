66c2c095c6cdee73cc0300eead24bad64e8da1f3
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-12-29 13:01:44 -0800

    added coloring

--- _tracks.py
+++ _tracks.py
@@ -1,64 +1,92 @@
 
-import napari
 import numpy as np
 import drawsvg as dw
 
 from tracks_3d import viewer
 
-from napari._vispy.visuals.tracks import TracksVisual
+from napari.layers import Tracks
+from napari._vispy.filters.tracks import TracksFilter
 from vispy.visuals.line import LineVisual
+
+from napari.viewer import current_viewer
+
+
+def _get_track_filter(line_visual: LineVisual) -> TracksFilter:
+    filters = line_visual._subvisuals[0]._filters
+    for f in filters:
+        if isinstance(f, TracksFilter):
+            return f
+    raise ValueError(f"No 'TracksFilter' found in {filters}")
+
+
+def tracks2svg(layer: Tracks) -> dw.Drawing:
+
+    viewer = current_viewer()
+
+    track_visual: LineVisual = viewer.window._qt_viewer.canvas.layer_to_visual[layer].node._subvisuals[0]
+    track_visual.update()
+
+    data2canvas = track_visual.get_transform(
+        map_from='visual', map_to='canvas'
+    )
+
+    canvas_data = data2canvas.map(track_visual._pos)
+    pos = canvas_data[:, :2]
+
+    connex = track_visual._connect
+    line_stops = np.where(~connex)[0] + 1
+
+    track_filter = _get_track_filter(track_visual)
+    current_t = track_filter.current_time
+    time = track_filter.vertex_time.ravel()
+    opacity = (track_filter.head_length + current_t - time) / (
+        track_filter.tail_length + track_filter.head_length
+    )
+    opacity = np.clip(1 - opacity, 0, 1)
+    opacity[time > current_t] = 0
+
+    height, width = viewer._canvas_size
+
+    track_ids = layer._manager.track_ids
+
+    d = dw.Drawing(width, height, id_prefix="tracks_")
+
+    start = 0
+    for stop in line_stops:
+        g = None
+        for i in range(start, stop - 1):
+            if opacity[i] < 1e-6:
+                continue
+            if g is None:
+                g = dw.Group(id=f"track_{track_ids[i]}")
+
+            rgb_color = "rgb({}, {}, {})".format(
+                *(track_visual.color[start, :3] * 255)
+            )
+            g.append(
+                dw.Line(
+                    *pos[i],
+                    *pos[i + 1],
+                    stroke=rgb_color,
+                    stroke_width=track_visual.width,
+                    stroke_opacity=opacity[i],
+                )
+            )
+        d.append(g)
+        start = stop
+
+    return d
 
 
 def main() -> None:
 
     layer = viewer.layers['tracks']
 
-    data = layer.data
-    node: LineVisual = viewer.window._qt_viewer.canvas.layer_to_visual[layer].node._subvisuals[0]
-    node.update()
-
-    data2canvas = node.get_transform(
-        map_from='visual', map_to='canvas'
-    )
-
-    canvas_data = data2canvas.map(node._pos)
-    pos = np.flip(canvas_data[:, :2], axis=1).copy()
-
-    connex = node._connect
-    line_stops = np.where(~connex)[0] + 1
-
-    print(node.color[:, -1].max())
-    print(node.color[:, -1].min())
-
-    width, height = pos.max(axis=0)
-
-    d = dw.Drawing(width, height)
-
-    start = 0
-    for stop in line_stops:
-        rgb_color = "rgb({}, {}, {})".format(
-            *(node.color[start, :3] * 255)
-        )
-        path = dw.Path(
-            fill='none',
-            stroke=rgb_color,
-            stroke_width=node.width,
-        )
-        path.M(*pos[start])
-        for p in pos[start + 1:stop]:
-            path.L(*p)
-        d.append(path)
-        start = stop
+    d = tracks2svg(layer)
 
     d.save_svg('pic.svg')
-    return
-
-    print(data.shape)
-    print(canvas_data)
-    viewer.add_points(canvas_data[:, :-2], size=5, name='canvas')
-
+    import napari
     napari.run()
-    pass
 
 
 if __name__ == '__main__':
